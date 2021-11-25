0367fa6c22f785e168faf428ddc85771d6e73640
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2021-11-25 21:30:26 +0530

    Table Creation.py
    This program creates the required tables that will be used in the main program for data storage in the database created using database creation file.

---
+++ Table creation.py
@@ -0,0 +1,14 @@
+#this program creates the required tables that will be used in the main program for data storage in the database created using database creation file.
+def table():
+    import mysql.connector as sql
+    mycon=sql.connect(host=hostname,user=username,passwd=pswd,database=databasename)
+    cursor=mycon.cursor()
+    mycon.autocommit=True
+    s1="create table railway(fname varchar(100), lname varchar(100), phno varchar(20), age int(4), gender varchar(1), fromf varchar(100), tod varchar(100), datefd date, username varchar(100), password varchar(100), trainexp varchar(50))"
+    cursor.execute(s1)
+    s2="create table useraccount(fname varchar(100), lname varchar(100), username varchar(100) primary key, password varchar(100), phno varchar(20), gender varchar(50), dob date, age varchar(4))"
+    cursor.execute(s2)
+hostname=input("Enter the host type:")
+username=input("Enter the username:")
+pswd=input("Enter the password:")
+table()
