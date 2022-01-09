2bbad4c13c9828c694636858ba32958e05570a7a
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2022-01-09 14:24:30 +0530

    Updated program after periodic testing

--- Online Railway Reservation Program.py
+++ Online Railway Reservation Program.py
@@ -1,25 +1,25 @@
 #This is the main program that will perform all the functions.
-def connectivity():
+def connectivity(hostname, username, pswd, databasename):
     import mysql.connector
     mycon=mysql.connector.connect (host=hostname,user=username, passwd=pswd,database=databasename)
     cursor=mycon.cursor()
     mycon.autocommit=True
 
 def menu():
-    print('1.YES')
-    print('2.NO')
-    ch=int(input('DO YOU WANT TO CONTINUE OR NOT:'))
+    print("1.YES")
+    print("2.NO")
+    ch=int(input("DO YOU WANT TO CONTINUE OR NOT:"))
     while ch==1:
-        print('WELECOME TO ONLINE RAILWAY RESERVATION SYSTEM')
-        print('1.SIGN IN')
-        print('2.SIGN UP')
-        print('3.DELETE ACCOUNT')
-        print('4.EXIT')
-        ch1=int(input('ENTER YOUR CHOICE:'))
+        print("WELECOME TO ONLINE RAILWAY RESERVATION SYSTEM")
+        print("1.SIGN IN")
+        print("2.SIGN UP")
+        print("3.DELETE ACCOUNT")
+        print("4.EXIT")
+        ch1=int(input("ENTER YOUR CHOICE:"))
         if ch1==1:
             a=sign_in()
             if a==True:
-                print('WELCOME')
+                print("WELCOME")
                 main()
             else:
                 print("ACCOUNT DOES NOT EXIST.")
@@ -29,35 +29,34 @@ def menu():
             if a==True:
                 main()
             else:
-                print('PASSWORD ALREADY EXISTS')
+                print("PASSWORD ALREADY EXISTS")
                 continue
         elif ch1==3:
             c=delete_account()
             if c==True:
-                print('ACCOUNT DELETED')
+                print("ACCOUNT DELETED")
                 continue
             else:
-                 print('YOUR PASSWORD OR USERNAME IS INCORRECT')
+                 print("YOUR PASSWORD OR USERNAME IS INCORRECT")
                  continue
 
         elif ch1==4:
-            print('THANK YOU')
+            print("THANK YOU")
             break
         else:
-            print('ERROR 404:PAGE NOT FOUND')
+            print("ERROR 404:PAGE NOT FOUND")
             break
 def main():
-    print('1.yes')
-    print('2.no')
-    c=1
+    print("1.YES")
+    print("2.NO")
+    c=int(input("DO YOU WANT TO CONTINUE OR NOT:"))
     while c!=2:
-        c=int(input("Do you want to continue or not:"))
-        print('1.TICKET BOOKING')
-        print('2.TICKET CHECKING')
-        print('3.TICKET CANCELLING')
-        print('4.ACCOUNT DETAILS')
-        print('5.LOG OUT')
-        ch=int(input('Enter your choice:'))
+        print("1.TICKET BOOKING")
+        print("2.TICKET CHECKING")
+        print("3.TICKET CANCELLING")
+        print("4.ACCOUNT DETAILS")
+        print("5.LOG OUT")
+        ch=int(input("ENTER YOUR CHOICE:"))
         if ch==1:
             ticket_booking()
         elif ch==2:
@@ -74,24 +73,24 @@ def main():
         
             
 def ticket_booking():
-    fnm=input('enter ticket booker first name:')
-    lnm=input('enter ticket booker last name:')
-    phno1=input('enter ticket booker phone number:')
-    age=int(input('enter ticket booker age:'))
-    print(' M=MALE','\n','F=FEMALE','\n','N=NOT TO MENTION')
-    gender=input('enter ticket booker gender M/F/N:')
+    fnm=input("Enter ticket booker first name:")
+    lnm=input("Enter ticket booker last name:")
+    phno1=input("Enter ticket booker phone number:")
+    age=int(input("Enter ticket booker age:"))
+    print("M=MALE","\n","F=FEMALE","\n","N=NOT TO MENTION")
+    gender=input("Enter ticket booker gender M/F/N:")
     while True:
         if gender in ("M","F","N","m","f","n"):
             break
         else:
             print("Error enter again")
-            gender=input('enter ticket booker gender M/F/N:')
+            gender=input("Enter ticket booker gender M/F/N:")
     v=gender.upper()
-    fr=input('enter ticket booker starting point:')
-    to=input('enter ticket booker destination:')
-    date1=input('enter travel date(dd):')
-    date2=input('enter travel month(mm):')
-    date3=input('enter travel year(yyyy):')
+    fr=input("Enter ticket booker starting point:")
+    to=input("Enter ticket booker destination:")
+    date1=input("Enter travel date(dd):")
+    date2=input("Enter travel month(mm):")
+    date3=input("Enter travel year(yyyy):")
     date=date3+"-"+date2+"-"+date1
     print(""" SELECT TRAIN:
 1. KARNATAKA EXPRESS
@@ -100,29 +99,29 @@ def ticket_booking():
 4. DURONTO EXPRESS
 5. JAN SHATABDI EXPRESS
 6. SAMPARK KRANTI EXPRESS""")
-    tr1=int(input("ENTER YOUR CHOICE 1/2/3/4/5/6:"))
+    tr1=int(input("Enter your choice 1/2/3/4/5/6:"))
     while True:
         if tr1 in (1,2,3,4,5,6):
             break
         else:
             print("Error. Enter again")
-            tr1=int(input("ENTER YOUR CHOICE 1/2/3/4/5/6:"))
+            tr1=int(input("Enter your choice 1/2/3/4/5/6:"))
     tr2={1:'KARNATAKA EXPRESS',2:'RAJDHANI EXPRESS',3:'SHATABDI EXPRESS',4:'DURONTO EXPRESS',5:'JAN SHATABDI EXPRESS',6:'SAMPARK KRANTI EXPRESS'}
     train=tr2[tr1]
     usn=input("ENTER YOUR USERNAME:")
     pswd=input("ENTER YOUR PASSWORD:")
     s1="insert into railway values('{}','{}','{}',{},'{}','{}','{}','{}','{}','{}','{}')".format(fnm,lnm,phno1,age,v,fr,to,date,usn,pswd,train)
     cursor.execute(s1)
-    print('BOOKED SUCCESSFULLY')
+    print("BOOKED SUCCESSFULLY")
 
 def ticket_checking():
-    print('1.yes')
-    print('2.no')
-    ch=int(input("do you want to continue or not:"))
+    print('1.Yes')
+    print('2.No')
+    ch=int(input("Do you want to continue or not:"))
     if ch==1:
-        usn=input('enter your username:')
-        pswd=input('enter your password:')
-        phno1=input('enter your phone number:')
+        usn=input("Enter your username:")
+        pswd=input("Enter your password:")
+        phno1=input('Enter your phone number:")
         try:
             s1="select * from railway where username='{}' and password='{}' and phno='{}'".format(usn,pswd,phno1)
             cursor.execute(s1)
@@ -142,42 +141,42 @@ def ticket_checking():
                 print(a[8],'::::',Data[10])
  
         except:
-            print('TICKET DOES NOT EXISTS')
+            print("TICKET DOES NOT EXISTS")
     elif ch==2:
-        print('THANK YOU')
+        print("THANK YOU")
     else:
-        print('ERROR 404:PAGE NOT FOUND')
+        print("ERROR 404:PAGE NOT FOUND")
 
 def ticket_cancelling():
-    print('1.yes')
-    print('2.no')
-    ch=int(input("do you want to continue or not:"))
+    print("1.Yes")
+    print("2.No")
+    ch=int(input("Do you want to continue or not:"))
     if ch==1:
-        fnam=input('enter the ticket holder first name:')
-        lnam=input('enter the ticket holder last name:')
-        date1=input('enter travel date(dd):')
-        date2=input('enter travel month(mm):')
-        date3=input('enter travel year(yyyy):')
+        fnam=input("Enter the ticket holder first name:")
+        lnam=input("Enter the ticket holder last name:")
+        date1=input("Enter travel date(dd):")
+        date2=input("Enter travel month(mm):")
+        date3=input("Enter travel year(yyyy):")
         tdate=date3+"-"+date2+"-"+date1
         s1="delete from railway where fname='{}' and lname='{}' and datefd='{}'".format(fnam,lnam,tdate)
         cursor.execute(s1)
-        print('TICKET CANCELLED')
+        print("TICKET CANCELLED")
     elif ch==2:
-        print('THANK YOU')
+        print("THANK YOU")
     else:
-        print('ERROR 404:PAGE NOT FOUND')
+        print("ERROR 404:PAGE NOT FOUND")
 
 def delete_account():
-    a=input('USER NAME:')
-    b=input('PASS WORD:')
-    c=input('FIRST NAME:')
+    a=input("USER NAME:")
+    b=input("PASS WORD:")
+    c=input("FIRST NAME:")
     try:
         s1="select fname,lname from useraccount where username='{}'".format(a)
         cursor.execute(s1)
         data=cursor.fetchall()[0]
         data=list(data)
         if data[0]==c:
-            print('IS THIS YOUR ACCOUNT')
+            print("IS THIS YOUR ACCOUNT")
             c1="select fname,lname from useraccount where username='{}'".format(a)
             cursor.execute(c1)
             data1=cursor.fetchall()[0]
@@ -207,52 +206,52 @@ def delete_account():
                     cursor.execute(b1)
                     return True
                 elif vi==2:
-                    print('SORRY,RETRY')
+                    print("SORRY,RETRY")
                 else:
-                    print('ERROR 404:PAGE NOT FOUND')
+                    print("ERROR 404:PAGE NOT FOUND")
         else:
             return False
     except:
-        print('ACCOUNT DOES NOT EXIST')
+        print("ACCOUNT DOES NOT EXIST")
 
 def sign_up():
     f=input("FIRST NAME:")
     l=input("LAST NAME:")
-    a=input('USER NAME:')
-    b=input('PASS WORD:')
-    c=input('RE-ENTER YOUR PASS WORD:')
+    a=input("USER NAME:")
+    b=input("PASS WORD:")
+    c=input("RE-ENTER YOUR PASS WORD:")
     if b==c:
         ph=input("PHONE NUMBER:")
-        print(' M=MALE','\n','F=FEMALE','\n','N=NOT TO MENTION')
-        gen=input('ENTER YOUR GENDER M/F/N:')
+        print("M=MALE","\n","F=FEMALE","\n","N=NOT TO MENTION")
+        gen=input("ENTER YOUR GENDER M/F/N:")
         while True:
             if gen in ("M","F","N","m","f","n"):
                 break
             else:
-                print("Error enter again")
-                gen=input('enter your gender M/F/N:')
+                print("ERROR. ENTER AGAIN")
+                gen=input("ENTER YOUR GENDER M/F/N:")
         v=gen.upper()
         print("ENTER YOR DATE OF BIRTH")
         d=input("DD:")
         o=input("MM:")
         p=input("YYYY:")
         dob=p+'-'+o+'-'+d
-        age=input('YOUR AGE:')
+        age=input("YOUR AGE:")
         
         try:
             c1="insert into useraccount values('{}','{}','{}','{}','{}','{}','{}','{}')".format(f,l,a,b,ph,v,dob,age)
             cursor.execute(c1)
-            print('WELCOME',f,l)
+            print("WELCOME",f,l)
             return True
         except:
-            print('USERNAME ALREADY EXISTS')
+            print("USERNAME ALREADY EXISTS")
             return False
     else:
-        print('BOTH PASSWORDS ARE NOT MATCHING')
+        print("BOTH PASSWORDS ARE NOT MATCHING")
 def sign_in():
-    a=input('USER NAME:')
-    b=input('PASS WORD:')
-    c=input('FIRST NAME:')
+    a=input("USER NAME:")
+    b=input("PASS WORD:")
+    c=input("FIRST NAME:")
     try:
         s1="select fname from useraccount where username='{}'".format(a)
         c1="select fname,lname from useraccount where username='{}'".format(a)
@@ -264,16 +263,16 @@ def sign_in():
         data=cursor.fetchall()[0]
         data=list(data)[0]
         if data==c:
-            print(' HII ',data1)
+            print(" HII ",data1)
             return True
         else:
             return False
     except:
-        print('ACCOUNT DOES NOT EXIST')
+        print("ACCOUNT DOES NOT EXIST")
         
 def account_details():
-    a=input('USER NAME:')
-    b=input('PASS WORD:')
+    a=input("USER NAME:")
+    b=input("PASS WORD:")
     try:
         s1="select password from useraccount where username='{}'".format(a)
         c1="select fname,lname from useraccount where username='{}'".format(a)
@@ -300,16 +299,16 @@ def account_details():
 
 
         else:
-            print("password doesn't match")
+            print("PASSWORD DOESN'T MATCH")
             return False
     except:
-        print('ACCOUNT DOES NOT EXIST')
+        print("ACCOUNT DOES NOT EXIST")
         
 hostname=input("Enter the host type:")
 username=input("Enter the username:")
 pswd=input("Enter the password:")
 databasename=input("Enter the name of database:")
-connectivity()       
+connectivity(hostname, username, pswd, databasename)       
 menu()
 
 
