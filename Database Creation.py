df2e51f0f88820ec8c514dd28f62b20c1dc24164
Ananya Gupta <90386813+Ananya2003Gupta@users.noreply.github.com>
2022-01-09 13:53:26 +0530

    Updated function database

--- Database Creation.py
+++ Database Creation.py
@@ -1,12 +1,13 @@
 #This program will perform the operation to create database.
 
-def database():
+def database(hostname,username,pswd,databse):
     import mysql.connector as sql
     conn=sql.connect(host=hostname,user=username,passwd=pswd)
     mycursor=conn.cursor()
-    s="create database railway"
+    s="create database '{}'".format(databse)
     mycursor.execute(s)
 hostname=input("Enter the host type:")
 username=input("Enter the username:")
 pswd=input("Enter the password:")
-database()
+databse=input("Enter the name you want to give to your database eg-railway :")
+database(hostname,username,pswd,databse)
